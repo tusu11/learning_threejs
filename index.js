7cc94115fe798bec1aa3e6e3f97f6bf7ec738796
tusu11 <owata_110@yahoo.co.jp>
2020-04-13 19:36:44 +0900

    cra

--- index.js
+++ index.js
@@ -12,40 +12,59 @@ function threeStart(){
 let renderer, scene, canvasFrame
 function initThree(){
   canvasFrame = document.getElementById('canvas-frame')
-  //renderer = new THREE.WebGLRenderer()
   renderer = new THREE.WebGLRenderer()
   if(!renderer) alert('three.jsの初期化に失敗')
   renderer.setSize(canvasFrame.clientWidth, canvasFrame.clientHeight)
   canvasFrame.appendChild(renderer.domElement)
   renderer.setClearColor(0x000000, 1.0)
   scene = new THREE.Scene()
-  
 }
 
 let camera
 function initCamera(){
   camera = new THREE.PerspectiveCamera(45, canvasFrame.clientWidth/canvasFrame.clientHeight, 1, 1000)
-  camera.position.set(10, 10, 20)
+  camera.position.set(40, 40, 80)
   camera.up.set(0, 0, 1)
   camera.lookAt(0, 0, 0)
 }
 
 let axis
-let grid
-let ring
+let arrows
 function initObject(){
-  axis = new THREE.AxesHelper(50)
-  scene.add(axis)
-  axis.position.set(0, 0, 0)
-  grid = new THREE.GridHelper(50, 10)
-  grid.rotation.set(Math.PI/2, 0, 0)
-  scene.add(grid)
-
-  const geometry = new THREE.ConvexGeometry()
-  const material = new THREE.MeshNormalMaterial({color: 0xffff00, side: THREE.DoubleSide, wireframe: true})
-  ring = new THREE.Mesh(geometry, material)
-
-  scene.add(ring)
+  arrows = new THREE.Group()
+  arrows.add(
+    new THREE.ArrowHelper(
+      new THREE.Vector3(1, 0, 0),
+      new THREE.Vector3(0, 0, 0),
+      30,
+      0xFF0000,
+      5,
+      2
+    )
+  )
+  arrows.add(
+    new THREE.ArrowHelper(
+      new THREE.Vector3(0, 1, 0),
+      new THREE.Vector3(0, 0, 0),
+      30,
+      0x00FF00,
+      5,
+      2
+    )
+  )
+  arrows.add(
+    new THREE.ArrowHelper(
+      new THREE.Vector3(0, 0, 1),
+      new THREE.Vector3(0, 0, 0),
+      30,
+      0x0000FF,
+      5,
+      2
+    )
+  )
+  scene.add(arrows)
+  arrows.position.set(5, 15, 10)
+  arrows.rotation.set(Math.PI/6, Math.PI/6, Math.PI/6)
 }
 
 function draw(){
