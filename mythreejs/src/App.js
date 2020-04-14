2da30a3fac38190c15fcb894ce615c150aa32257
tusu11 <owata_110@yahoo.co.jp>
2020-04-15 03:14:45 +0900

    今までのファイルをCRAプロジェクトに統合

--- mythreejs/src/App.js
+++ mythreejs/src/App.js
@@ -2,31 +2,32 @@ import React from 'react';
 //import ReactDOM from 'react-dom';
 import Sidebar from 'react-sidebar';
 //import List from './List'
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { Route, Link, Redirect } from 'react-router-dom';
+// import { Route, Link, Redirect } from 'react-router-dom';
+// import { Helmet } from 'react-helmet'
 import MainContent from './MainContent';
 import data from './content.json'
 
 
 const mql = window.matchMedia(`(min-width: 800px)`);
 
-const TopPage = () => (
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn Three.js
-    </a>
-  </header>
-)
+// const TopPage = () => (
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.js</code> and save to reload.
+//     </p>
+//     <a
+//       className="App-link"
+//       href="https://reactjs.org"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       Learn Three.js
+//     </a>
+//   </header>
+// )
 
 const List = (props) => (
   <div>
@@ -74,8 +75,6 @@ class App extends React.Component {
 
   handleContentChanged(e) {
     this.setState({pageTitle: e.charAt(0).toUpperCase() + e.slice(1)})  //頭文字を大文字に
-    console.log(this.state)
-
   }
 
   render() {
@@ -88,9 +87,8 @@ class App extends React.Component {
           onSetOpen={this.onSetSidebarOpen}
           sidebarClassName="sidebar"
         >
-          <Link  to="/test"></Link>
-          <Route path="/test" component={() => <MainContent />}/>
-          <Route exact path="/" component={() => <Redirect to="/top" />}/>
+          {/*<Link  to="/test">gfdg</Link>*/}
+          <MainContent title={this.state.pageTitle} />
         </Sidebar>
       </div>
     );
--- mythreejs/src/MainContent.js
+++ mythreejs/src/MainContent.js
@@ -2,106 +2,54 @@ import React from 'react';
 // import Sidebar from 'react-sidebar';
 // import List from './List'
 import './App.css';
-import page from './pages/1-3.html'
+import { Helmet } from 'react-helmet';
 
 class MainContent extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-    };
-    this.handleContentChanged = this.handleContentChanged.bind(this)
+
+    }
   }
 
   componentDidMount() {
+    console.log("call did m")
 
   }
 
-  componentWillUnmount() {
+  componentDidUpdate() {
+    //canvasノードを削除
+    // let canvasFrame = document.getElementById('canvas-frame')
+    // canvasFrame.innerHTML = ''
+    // window.threeStart()
 
+    console.log("call did up")
+    // window.threeStart()
+
+    //Eventlistenerを削除
+    //window.removeEventListener("keydown", arguments.callee)
   }
 
-  handleContentChanged() {
-
-  }
-
-  render() {
+  render(){
     return (
-      <div className="Main">
-        <p>hello, world</p>
-        <html lang="ja">
-          <head>
-            <meta charset="utf-8" />
-            <title>トーラスオブジェクト</title>
-            <script src="three.js-master/build/three.js"></script>
-            <script type="text/javascript">{
-              window.addEventListener("load", function(){
-                threeStart()
-              })
-
-              function threeStart(){
-                initThree()
-                initCamera()
-                initObject()
-                draw()
-              }
-
-              let renderer, scene, canvasFrame
-              function initThree(){
-                canvasFrame = document.getElementById('canvas-frame')
-                //renderer = new THREE.WebGLRenderer()
-                renderer = new THREE.WebGLRenderer()
-                if(!renderer) alert('three.jsの初期化に失敗')
-                renderer.setSize(canvasFrame.clientWidth, canvasFrame.clientHeight)
-                canvasFrame.appendChild(renderer.domElement)
-                renderer.setClearColor(0x000000, 1.0)
-                scene = new THREE.Scene()
-                
-              }
-
-              let camera
-              function initCamera(){
-                camera = new THREE.PerspectiveCamera(45, canvasFrame.clientWidth/canvasFrame.clientHeight, 1, 1000)
-                camera.position.set(100, 100, 200)
-                camera.up.set(0, 0, 1)
-                camera.lookAt(0, 0, 0)
-              }
-
-              let axis
-              let grid
-              let torus
-              function initObject(){
-                axis = new THREE.AxesHelper(50)
-                scene.add(axis)
-                axis.position.set(0, 0, 0)
-                grid = new THREE.GridHelper(50, 10)
-                grid.rotation.set(Math.PI/2, 0, 0)
-                scene.add(grid)
-
-                let geometry = new THREE.TorusGeometry(40, 10, 50, 50, 2*Math.PI*3/4)
-                let material = new THREE.MeshNormalMaterial({side: THREE.DoubleSide, wireframe: false})
-                torus = new THREE.Mesh(geometry, material)
-                //フラットシェーディング
-                for(let i=0; i<geometry.faces.length; i++){
-                  geometry.faces[i].vertexNormals[0].copy(geometry.faces[i].normal)
-                  geometry.faces[i].vertexNormals[1].copy(geometry.faces[i].normal)
-                  geometry.faces[i].vertexNormals[2].copy(geometry.faces[i].normal)
-                }
-
-                scene.add(torus)
-              }
-
-              function draw(){
-                //requestAnimationFrame(draw)
-                renderer.render(scene, camera)
-              }
-            }
-            </script>
-          </head>
-          <body>
-            <div id="canvas-frame"></div>
-          </body>
-        </html>
-      </div>
+      <React.Fragment>
+        <Helmet
+          title={this.props.title}
+          defaultTitle={'my three.js'}
+          // script={[
+          //   {src: "./pages/js/" + this.props.title + ".js" , type: "text/javascript"} // jsonで渡せば良かった
+          // ]}
+        >
+          {/* {(this.props.title !== 'top') &&
+            <script type="text/javascript" src={"./pages/js/" + this.props.title + ".js"}></script>
+          } */}
+        </Helmet>
+        <h1>
+          {this.props.title}
+        </h1>
+        <iframe title="inlineFrame" src={"./temp/" + this.props.title + ".html"} width="512px" height="512px"></iframe>
+        {/* <div id="canvas-frame"></div> */}
+      </React.Fragment>
     );
   }
 }
